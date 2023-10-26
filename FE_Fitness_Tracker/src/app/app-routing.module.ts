46a060f01eb5729177ddd085b355087b0aaf56e4
Evangelia Panteliadou <evangelia.panteliadou@gmail.com>
2023-10-26 22:03:54 +0200

    Creation of components sign-up and sign-in w/o auth global css in styles.css

--- FE_Fitness_Tracker/src/app/app-routing.module.ts
+++ FE_Fitness_Tracker/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'dashboard',
+    component: DashboardComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'about', component: AboutComponent },
   // Weitere Routen, wenn vorhanden
 ];
--- FE_Fitness_Tracker/src/app/app.module.ts
+++ FE_Fitness_Tracker/src/app/app.module.ts
@@ -4,17 +4,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'dashboard', component: DashboardComponent },
   // Füge weitere Routen hinzu, wenn nötig
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashboardComponent,
+    ForgotPasswordComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
