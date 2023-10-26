9ae0799fa8e091f057681775283783dd75821dec
Kristinaliebhart <kristinalie@edu.aau.at>
2023-10-26 11:04:12 +0200

    add about page

--- FE_Fitness_Tracker/src/app/app.module.ts
+++ FE_Fitness_Tracker/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  // Füge weitere Routen hinzu, wenn nötig
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
