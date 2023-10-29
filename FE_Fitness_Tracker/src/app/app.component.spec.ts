0b1c8b338e63c98f3c823d017e4ba70b3cbd72b9
Kristinaliebhart <kristinalie@edu.aau.at>
2023-10-29 11:54:58 +0100

    add unit tests for app component

--- FE_Fitness_Tracker/src/app/app.component.spec.ts
+++ FE_Fitness_Tracker/src/app/app.component.spec.ts
@@ -20,10 +20,52 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('FE_Fitness_Tracker');
   });
 
-  it('should render title', () => {
+  it('should have a logo in the title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FE_Fitness_Tracker app is running!');
+    const app = fixture.nativeElement as HTMLElement;
+    const logo = app.querySelector('.logo') as HTMLImageElement;
+    expect(logo).toBeTruthy();
+    expect(logo.src).toContain('/assets/FITJourney_logo.png');
+  });
+  
+  it('should have links for dashboard page', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.nativeElement as HTMLElement;
+    const dashboard = app.querySelector('[routerLink="/dashboard"]');
+    expect(dashboard).toBeTruthy();
+  });
+
+  it('should have a link for the profile page', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.nativeElement as HTMLElement;
+    const profile = app.querySelector('[routerLink="/profile"]');
+    expect(profile).toBeTruthy();
+  });
+  
+  it('should have a link for about page', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.nativeElement as HTMLElement;
+    const about = app.querySelector('[routerLink="/about"]');
+    expect(about).toBeTruthy();
+  });
+
+  it('should have a header', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.nativeElement as HTMLElement;
+    const header = app.querySelector('.header');
+    expect(header).toBeTruthy();
+  });
+
+  it('should have a footer', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.nativeElement as HTMLElement;
+    const footer = app.querySelector('.footer');
+    expect(footer).toBeTruthy();
   });
 });
