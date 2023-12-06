be50bbc8043e9f4eb6158b3e78062614edb397d5
Evangelia Panteliadou <evangelia.panteliadou@gmail.com>
2023-12-06 21:20:51 +0100

    refactor sign-up

--- FE_Fitness_Tracker/src/app/components/sign-up/sign-up.component.ts
+++ FE_Fitness_Tracker/src/app/components/sign-up/sign-up.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css'],
+  styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
-  ngOnInit() {}
+export class SignUpComponent {
+
+  onSubmitSignUp(form: NgForm): void {
+    if (form.valid) {
+      // Your sign-up logic goes here
+      console.log('Form submitted successfully!');
+      // You can access form.value to get the form data
+      // For example: const email = form.value.userEmail;
+      //              const username = form.value.username;
+      //              const password = form.value.password;
+    } else {
+      // Handle validation errors
+      console.log('Form has validation errors!');
+    }
+  }
 }
