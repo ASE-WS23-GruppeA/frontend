e61ade399a59ef6233726a83b73b0f078c825861
Kristinaliebhart <kristinalie@edu.aau.at>
2024-01-09 11:09:47 +0100

    implement first get request

--- FE_Fitness_Tracker/src/app/components/dashboard/dashboard.service.ts
+++ FE_Fitness_Tracker/src/app/components/dashboard/dashboard.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
+  private url = 'TODO';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  //GET user progress
+  //GET /user-progress/{userId}
+  getUserProgress(userId: string): Observable<any> {
+    return this.http.get(`${this.url}/user-progress/${userId}`);
+  }
+
 }
