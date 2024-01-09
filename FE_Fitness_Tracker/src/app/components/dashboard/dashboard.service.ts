8944155502abb0159c0831852519e77f182ae8e4
Kristinaliebhart <kristinalie@edu.aau.at>
2024-01-09 11:15:32 +0100

    implement get endpoints for analytics

--- FE_Fitness_Tracker/src/app/components/dashboard/dashboard.service.ts
+++ FE_Fitness_Tracker/src/app/components/dashboard/dashboard.service.ts
@@ -15,4 +15,26 @@ export class DashboardService {
     return this.http.get(`${this.url}/user-progress/${userId}`);
   }
 
+  //Get workout progress
+  //GET /workout-progress/{workoutId}
+  getWorkoutProgress(workoutId: string): Observable<any> {
+
+    return this.http.get(`${this.url}/workout-progress/${workoutId}`);
+
+  }
+
+  //Get Exercise ProgresS
+  //GET /exercise-progress/{exerciseId}
+  getExerciseProgress(exerciseId: string): Observable<any> {
+
+    return this.http.get(`${this.url}/exercise-progress/${exerciseId}`);
+
+  }
+
+  //Get Total volume
+  //GET /total-volume/{userId}
+  getTotalVolume(userId: string): Observable<any> {
+    return this.http.get(`${this.url}/total-volume/${userId}`);
+  }
+
 }
